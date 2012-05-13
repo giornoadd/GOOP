0d948a95c653d5005a578486316245a4685601d1
giornoadd <giornoadd@NBOAHP103014.(none)>
2012-05-13 11:26:26 +0700

    first commit

---
+++ test.java
@@ -0,0 +1,10 @@
+public class test{
+	public static void main(String[] args){
+		System.out.println("test");
+		Integer i1 = new Integer(999);
+		Integer i2 = new Integer(999);
+
+		System.out.println(i1 == i2);
+		System.out.println(i1.equals(i2));
+	}
+}
